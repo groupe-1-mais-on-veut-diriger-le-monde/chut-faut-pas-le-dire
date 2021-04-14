25e2631ceee57705bae793afb8ae9014214007dd
Edin Sulejmani <edin.sulejmani@etu.unige.ch>
2021-04-14 10:01:56 +0200

    add project vide for yaml config

---
+++ src/main/java/com/groupe1/groupe1_test/Groupe1TestApplication.java
@@ -0,0 +1,13 @@
+package com.groupe1.groupe1_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupe1TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Groupe1TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupe1/groupe1_test/Groupe1TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe1.groupe1_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Groupe1TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
