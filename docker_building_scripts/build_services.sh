5dfae5a8b5cecf31f22dfb8b1ad3f9ad36186f8c
Edin Sulejmani <edin.sulejmani@etu.unige.ch>
2021-04-28 12:51:23 +0200

    test sudo docker

--- docker_building_scripts/build_services.sh
+++ docker_building_scripts/build_services.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # build les images a orator du dockerfile
-docker image build -t service_film_build -f services/test_film/Dockerfile .
+sudo docker image build -t service_film_build -f services/test_film/Dockerfile .
 
 # connection au docker hub
-echo ${DockerPassword} | docker login --username ${DockerUsername} --password-stdin
+sudo echo ${DockerPassword} | docker login --username ${DockerUsername} --password-stdin
 
 # change les noms des images et les envoies au docker hub
-docker tag service_film_build edin0/service_film_build:latest
+sudo docker tag service_film_build edin0/service_film_build:latest
 
-docker push edin0/service_film_build:latest
+sudo docker push edin0/service_film_build:latest
 
 # test the build
 
-docker run -p 8080:8080 -d edin0/service_film_build
+sudo docker run -p 8080:8080 -d edin0/service_film_build
 
 #check website
 
-curl localhost:8080
+sudo curl localhost:8080
