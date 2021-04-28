f44a133c3e1547a2e989013916f46077a37f5d94
Edin Sulejmani <edin.sulejmani@etu.unige.ch>
2021-04-28 14:04:06 +0200

    login before tag

--- docker_building_scripts/build_services.sh
+++ docker_building_scripts/build_services.sh
@@ -4,7 +4,10 @@
 # build les images a partir du dockerfile
 sudo docker image build -t service_film_build -f ../services/test_film/Dockerfile .
 
+# connection au docker hub
+sudo echo ${DockerPassword} | docker login --username ${DockerUsername} --password-stdin
 
+Sudo docker login -u edin0
 
 # change les noms des images et les envoies au docker hub
 sudo docker tag service_film_build edin0/service_film_build:latest
