b880e502151bea537a2f1d1bd35863371eeb4a63
Edin Sulejmani <edin.sulejmani@etu.unige.ch>
2021-04-28 14:12:57 +0200

    test mdp no cat

--- docker_building_scripts/build_services.sh
+++ docker_building_scripts/build_services.sh
@@ -4,14 +4,19 @@
 # build les images a partir du dockerfile
 sudo docker image build -t service_film_build -f ../services/test_film/Dockerfile .
 
-# connection au docker hub
-#sudo echo ${DockerPassword} | docker login --username ${DockerUsername} --password-stdin
 
-cat ~/my_password.txt | sudo docker login --username edin0 --password-stdin
 
 # change les noms des images et les envoies au docker hub
 sudo docker tag service_film_build edin0/service_film_build:latest
 
+
+# connection au docker hub
+sudo echo ${DockerPassword} | docker login --username ${DockerUsername} --password-stdin
+
+
+sudo docker login --username edin0 --password-stdin
+
+# push images tagged
 sudo docker push edin0/service_film_build:latest
 
 # test the build
