df0eea8b9e2c0ae71cfbb12fc890c85a54dc2263
Jan <jan.dirkx@etu.unige.ch>
2021-05-14 19:51:40 +0200

    k

--- docker-compose/test-data/profileSQL/profile_test_data.sql
+++ docker-compose/test-data/profileSQL/profile_test_data.sql
@@ -30,5 +30,5 @@ create table Profile (
 drop sequence if exists seq_id;
 CREATE SEQUENCE seq_id START WITH 1 INCREMENT BY 1;
 INSERT INTO Profile ( id,name,age,  comedy,  sci, Horror, romance, action, thriller, drama, mystery, crime, animation, adventure, fantasy, comdedy_Romance, action_comedy, superhero, voir,Group1,Group2,Group3,Group4,Group5) VALUES (seq_id.nextval, 'jan',24, 1,  2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 'Avatar',1,2,3,4,5);
-INSERT INTO Profile ( id,name,age,  comedy,  sci, Horror, romance, action, thriller, drama, mystery, crime, animation, adventure, fantasy, comdedy_Romance, action_comedy, superhero, voir,Group1,Group2,Group3,Group4,Group5) VALUES (seq_id.nextval, 'Atoine',22, 11,  21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131, 141, 151, 'Toto',6,7,8,9,10);
+INSERT INTO Profile ( id,name,age,  comedy,  sci, Horror, romance, action, thriller, drama, mystery, crime, animation, adventure, fantasy, comdedy_Romance, action_comedy, superhero, voir,Group1,Group2,Group3,Group4,Group5) VALUES (seq_id.nextval, 'Atoine',22, 11,  21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131, 141, 151,'Toto',6,7,8,9,10);
 
