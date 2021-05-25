1694e1752e81a73d5fd846d646ba5df1d15918f3
Joao-Quinta <56771749+Joao-Quinta@users.noreply.github.com>
2021-05-25 22:41:21 +0200

    api tests

--- web-ui/src/app/APIfunctions.js
+++ web-ui/src/app/APIfunctions.js
@@ -1,42 +1,76 @@
-var baseUrl = "/jsonFiles/";
-var getUserById = "user=";
-var getGroupById = "group=";
-var getMovieById = "film=";
+/*
+var url_group = "http://129.194.10.126:30001/Groups";
+var url_profile = "http://129.194.10.126:30002/Profile";
+*/
 
-async function getJson(type, id) {
+async function creatJson(type, body){
+    //gets correct url
+    var url = getURL(type);
 
-    var url = "";
+    var response = await fetch(url,{
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(body)
+    });
+    var returnedId = await response.json();
 
-    switch(type) {
-        case "user":
-            url = baseUrl + getUserById + id + ".json";
-            console.log("getting user");
-            break;
-        case "group":
-            url = baseUrl + getGroupById + id + ".json";
-            console.log("getting group");
-            break;
-        case "film":
-            url = baseUrl + getMovieById + id + ".json";
-            console.log("getting movie");
-            break;
-        default:
-            console.log("bad getJson input !!");
-            return null;
-    }
+    return returnedId;
+}
+
+async function getJson(type, id){
+    //gets correct url
+    var url = getURL(type) + '/id/' + id.toString();
 
     var response = await fetch(url);
     var jsonData = await response.json();
+
     return jsonData;
-    
 }
 
-// TO DO
-function createGroup(){
-    console.log("create Grp");
+async function patchJson(type, id, body){
+    //gets correct url
+    var url = getURL(type) + '/id/' + id.toString();
+
+    var response = await fetch(url,{
+        method: 'PATCH',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(body)
+    });
+    var returnedId = await response.json();
+
+    return returnedId;
 }
 
-// TO DO
-function joinGroup(ID){
-    console.log("joining grp id " + ID);
+//deletes all entries of a DB -> might delete this function
+async function deleteAll(type){
+    //gets correct url
+    var url = getURL(type);
+    var currentUser = '';
+
+    var response = await fetch(url);
+    var jsonData = await response.json();
+
+    for (var i=0; i<jsonData.length; i++){
+        currentUser = jsonData[i].id.toString();
+        var deleteUrl = url + '/delete/' + currentUser;
+        await fetch(deleteUrl,{
+            method: 'DELETE'
+        });
+    }
+}
+
+// simple switch for user or grp url
+function getURL(type){
+    switch(type) {
+        case 'user':
+            var url = 'http://129.194.10.126:30002/Profile';
+            break;
+        case 'group':
+            var url = 'http://129.194.10.126:30001/Groups';
+            break;
+        default:
+            var url = '';
+            break;
+    }
+    return url;
 }
--- web-ui/src/app/index_1.js
+++ web-ui/src/app/index_1.js
@@ -1,5 +1,35 @@
-var testID = "1";
-
-
+/*var testID = "1";
 getJson("user", testID)
     .then((user) => choseMainScreen(user));
+*/
+deleteAll('group');
+deleteAll('user');
+
+
+var body = {
+    name: 'Joao Quinta',
+    age: 23
+}
+/*
+creatJson('user', body).then((userId) => console.log('user ID ' + userId));
+
+var body = {
+    name: 'nom_Du_Groupe',
+    host: 'Joao Quinta'
+}
+
+creatJson('group', body).then((groupId) => console.log('group ID ' + groupId));
+
+var body = {
+    name: 'Joao',
+    age: 23
+}*/
+
+var userId = async()=>{
+    var response = await creatJson('user', body);
+    return response ;
+}
+
+console.log(userId);
+
+//creatPatchJson('PATCH', 'user', body).then((userId) => console.log('user ID ' + userId));
